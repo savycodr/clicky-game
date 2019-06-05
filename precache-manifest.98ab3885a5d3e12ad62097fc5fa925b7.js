b516871e63a652dc0a52c888bcdfb0900142448e
savycodr <46404719+savycodr@users.noreply.github.com>
2019-06-04 22:11:18 -0400

    Updates

---
+++ precache-manifest.98ab3885a5d3e12ad62097fc5fa925b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df92abbe4081afd5fc384f61c9894c26",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "e6fa2d6a3530780e473a",
+    "url": "/clicky-game/static/css/main.d8cbf71d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clicky-game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e6fa2d6a3530780e473a",
+    "url": "/clicky-game/static/js/main.3c514f49.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
